44f7be358e1df60b5252ffdf2fb8a4c7754a3ff1
elisa lee <elisa.my.lee@gmail.com>
2016-09-18 23:35:39 -0500

    Edit jquery

--- landingPage.js
+++ landingPage.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
-
   function landingPage() {
     $('.enter').hide();
     $('.landing').show();
   };
+
+  function lpHoverExit(){
+    $(this).removeClass('hover');
+    $(this).closest('.enter').find('.logo').attr("src", "Images/emyl3_banner.svg");
+  }
 
   function hoverExit() {
     $(this).removeClass('hover');
     $(this).closest('.landing').find('.rotatingImg').attr("src", "Images/emyl3_banner.svg");
   }
 
-  $('.unclick').on('mouseleave', function(){
+  function hoverEnter() {
     $(this).addClass('hover');
-    $(this).closest('.enter').find('.logo').attr("src", "Images/emyl3_banner.svg");
-  });
-
-  $('.unclick').on('mouseover', function(){
-    $(this).addClass('hover');
-    $(this).closest('.enter').find('.logo').attr("src", "Images/emyl3_profilepic.svg");
-  });
-
-
-  $('.about').on('mouseover', function(){
-    $(this).addClass('hover');
-    $(this).closest('.landing').find('.rotatingImg').attr("src", "Images/emyl3_profilepic.svg");
-  });
-
-  $('.print').on('mouseover', function(){
-    $(this).addClass('hover');
-    $(this).closest('.landing').find('.rotatingImg').attr("src", "Images/emyl3_print.svg");
-  });
-
-
-  $('.web').on('mouseover', function(){
-    $(this).addClass('hover');
-    $(this).closest('.landing').find('.rotatingImg').attr("src", "Images/emyl3_web.svg");
-  });
+    var a = $(this).attr('class').split(' ')[0];
+    if(a == "about") {
+      $(this).closest('.landing').find('.rotatingImg').attr("src", "Images/emyl3_profilepic.svg");
+    }
+    else if(a == "print") {
+      $(this).closest('.landing').find('.rotatingImg').attr("src", "Images/emyl3_print.svg");
+    }
+    else if(a == "web") {
+      $(this).closest('.landing').find('.rotatingImg').attr("src", "Images/emyl3_web.svg");
+    }
+    else {
+      $(this).closest('.enter').find('.logo').attr("src", "Images/emyl3_profilepic.svg");
+    }
+  }
 
   $('.unclick').on('click', landingPage);
+  $('.unclick').on('mouseover', hoverEnter);
+  $('.unclick').on('mouseleave', lpHoverExit);
   $('.about').on('mouseleave', hoverExit);
+  $('.about').on('mouseover', hoverEnter);
   $('.print').on('mouseleave', hoverExit);
+  $('.print').on('mouseover', hoverEnter);
   $('.web').on('mouseleave', hoverExit);
-  });
+  $('.web').on('mouseover', hoverEnter);
+  $()
+});
--- script.js
+++ script.js
@@ -5,23 +5,26 @@ $(document).ready(function() {
     $(this).closest('.nav').find('.rotatingImg').attr("src", "Images/nav_emyl3.svg");
   }
 
-  $('.about').on('mouseover', function(){
+  function hoverEnter() {
     $(this).addClass('hover');
-    $(this).closest('.nav').find('.rotatingImg').attr("src", "Images/nav_about.svg");
-  });
+    var a = $(this).attr('class').split(' ')[0];
+    if(a == "about") {
+      $(this).closest('.nav').find('.rotatingImg').attr("src", "Images/nav_about.svg");
+    }
+    else if(a == "print") {
+      $(this).closest('.nav').find('.rotatingImg').attr("src", "Images/nav_print.svg");
+    }
+    else if(a == "web") {
+      $(this).closest('.nav').find('.rotatingImg').attr("src", "Images/nav_web.svg");
+    }
+  }
 
-  $('.print').on('mouseover', function(){
-    $(this).addClass('hover');
-    $(this).closest('.nav').find('.rotatingImg').attr("src", "Images/nav_print.svg");
-  });
-
-
-  $('.web').on('mouseover', function(){
-    $(this).addClass('hover');
-    $(this).closest('.nav').find('.rotatingImg').attr("src", "Images/nav_web.svg");
-  });
+  
 
   $('.about').on('mouseleave', hoverExit);
   $('.print').on('mouseleave', hoverExit);
   $('.web').on('mouseleave', hoverExit);
+  $('.about').on('mouseover', hoverEnter);
+  $('.print').on('mouseover', hoverEnter);
+  $('.web').on('mouseover', hoverEnter);
   });
